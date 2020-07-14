2b6e23577504d38be826ea5c5cc30560d0395973
guidumasperes <guidumasperes@hotmail.com>
2020-07-14 15:55:57 -0300

    Pegando os inputs dos campos de escrita, falta fazer a query

--- divit/src/Login.tsx
+++ divit/src/Login.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-
-function myFunc() {
-    alert("HI");
-}
+import React, { useState } from 'react';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function myFunc(user: any, pass: any) {
+        alert(user + pass);
+    }
+
     return (
         <div>
             <table>
                 <tbody>
-                    <tr><td>Email:</td></tr>
-                    <tr><td><input type="email"/></td></tr>
-                    <tr><td>Senha:</td></tr>
-                    <tr><td><input type="passwd"/></td></tr>
+                    <tr>
+                        <td>Email:</td>
+                    </tr>
                     <tr>
                         <td>
-                            <button onClick={myFunc}>Entrar</button>
-                            <button onClick={myFunc}>Cadastrar</button>
+                            <input type="email" onChange={(event) => setUsername(event.target.value)} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Senha:</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="passwd" onChange={(event) => setPassword(event.target.value)} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button onClick={() => myFunc(username, password)}>Entrar</button>
+                            <button onClick={() => myFunc(username, password)}>Cadastrar</button>
                         </td>
                     </tr>
                 </tbody>
@@ -30,15 +45,33 @@ function SingUp() {
         <div>
             <table>
                 <tbody>
-                    <tr><td>Email:</td></tr>
-                    <tr><td><input type="email"/></td></tr>
-                    <tr><td>Senha:</td></tr>
-                    <tr><td><input type="passwd"/></td></tr>
-                    <tr><td>Confirmar Senha:</td></tr>
-                    <tr><td><input type="passwd"/></td></tr>
+                    <tr>
+                        <td>Email:</td>
+                    </tr>
                     <tr>
                         <td>
-                            <button onClick={myFunc}>Cadastrar</button>
+                            <input type="email" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Senha:</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="passwd" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Confirmar Senha:</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="passwd" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button>Cadastrar</button>
                         </td>
                     </tr>
                 </tbody>
@@ -47,7 +80,4 @@ function SingUp() {
     );
 }
 
-export {
-    Login,
-    SingUp,
-}
+export { Login, SingUp };
