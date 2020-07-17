3b966288f12565540ef08d0fa9febf84f38cc270
victor Andrietta <vitorandrietta@gmail.com>
2020-07-17 03:25:41 -0300

    feat(groupCreation): add group creation screen

--- divit/src/Router.tsx
+++ divit/src/Router.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import {
-    BrowserRouter,
-    Switch,
-    Route,
-} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './App';
 import GroupListPage from './page/GroupListPage';
-import { Login, SingUp} from './Login';
+import { Login, SingUp } from './Login';
+import { GroupCreationpage } from './page/groupCreation';
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/groups/add" component={App} />
+                <Route path="/groups/add" component={GroupCreationpage} />
                 <Route path="/groups" component={GroupListPage} />
                 <Route path="/login">
                     <Login />
